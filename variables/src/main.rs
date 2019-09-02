f9d19ba1645f5156e2a4a4036c41c53332b72edf
skinth <ste0891@gmail.com>
2019-09-02 17:24:06 +0200

    tuples and arrays

--- variables/src/main.rs
+++ variables/src/main.rs
@@ -6,4 +6,22 @@ fn main() {
     x = 44;
 
     println!("Value of x: {}", x);
+
+    //destructuring compund variable
+    let tup = (500, 6.4, 1);
+    let (x, y, z) = tup;
+    println!("The value of y is: {}", y);
+    println!("The same value y: {}", tup.1);
+
+    let a = [1, 2, 3, 4, 5];
+    let mut ix = 0;
+    loop {
+        if (ix < 5) {
+            println!("a[{}]: {}", ix, a[ix]);
+            ix = ix +1;
+        } else {
+            break;
+        }
+    }
+
 }
