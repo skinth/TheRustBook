9fdb53845ebbe21a986e97c16c40e82bb7ddc557
skinth <ste0891@gmail.com>
2019-09-02 17:57:19 +0200

    expression and statement

--- functions/src/main.rs
+++ functions/src/main.rs
@@ -1,9 +1,16 @@
 fn main() {
     println!("Hello, world!");
 
-    another_func();
+    another_func(12345);
+
+    let exp = {
+        let a = 22;
+        a + 44
+    };
+
+    println!("I am an expression: {}", exp);
 }
 
-fn another_func() {
-    println!("I am another function!");
+fn another_func(x: i32) {
+    println!("I am another function! {}", x);
 }
