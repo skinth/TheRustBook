62d9f77e199ab81ee02c8d522ad598c693a9394a
skinth <ste0891@gmail.com>
2019-09-02 14:02:04 +0200

    end guessing game

--- guessing-game/src/main.rs
+++ guessing-game/src/main.rs
@@ -1,3 +1,35 @@
+use std::io;
+use rand::Rng;
+use std::cmp::Ordering;
+
 fn main() {
-    println!("Hello, world!");
+    println!("Guess the number!");
+
+    let secret = rand::thread_rng().gen_range(1, 101);
+
+    loop {
+        println!("Input your number");
+        let mut guess = String::new();
+        //&mut = mutable reference
+        io::stdin().read_line(&mut guess)
+            .expect("Failed to read line");
+
+        //Shadowing variable
+        let guess = match guess.trim().parse::<u32>() {
+            Ok(num) => num,
+            //_ is a "catchall" value
+            Err(_) => continue,
+        };
+
+        println!("You wrote: {}", guess);
+
+        match guess.cmp(&secret) {
+            Ordering::Less => println!("Too small!"),
+            Ordering::Equal => {
+                println!("Win!");
+                break;
+            },
+            Ordering::Greater => println!("Too big!"),
+        }
+    }
 }
